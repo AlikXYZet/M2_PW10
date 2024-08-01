5cec871242eeca852173a818bcd57360f5317399
Александр Осипов <alikxyzet@gmail.com>
2024-08-01 21:46:06 +0300

    10.1 Adding basic objects: * GeneratedCube; * Level "Main".

---
+++ Source/M2PW10/GeneratedCube.cpp
@@ -0,0 +1,79 @@
+﻿#include "GeneratedCube.h"
+
+AGeneratedCube::AGeneratedCube()
+{
+    // Вызывать Tick() каждый такт?
+    PrimaryActorTick.bCanEverTick = false;  // предварительно
+    //PrimaryActorTick.TickInterval = 1;    // 1 = 1 sec
+
+    /* ---   CubeMesh   --- */
+    CubeMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Cube Mesh"));
+    CubeMesh->SetCanEverAffectNavigation(false);
+
+    RootComponent = CubeMesh;
+    //--------------------------------------------
+
+
+
+    /* ---   Text   --- */
+    TextAge = CreateDefaultSubobject<UTextRenderComponent>(TEXT("Text Age"));
+    TextAge->SetupAttachment(RootComponent);
+    TextAge->SetTextRenderColor(FColor::Red);
+    TextAge->SetRelativeLocation(FVector(60.f, 30.f, 10.f));
+
+    TextLifetime = CreateDefaultSubobject<UTextRenderComponent>(TEXT("Text Lifetime"));
+    TextLifetime->SetupAttachment(RootComponent);
+    TextLifetime->SetTextRenderColor(FColor::Red);
+    TextLifetime->SetRelativeLocation(FVector(60.f, 30.f, -40.f));
+    //--------------------------------------------
+}
+
+void AGeneratedCube::BeginPlay()
+{
+    Super::BeginPlay();
+
+}
+
+void AGeneratedCube::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+}
+//----------------------------------------------------------------------------------------
+
+
+
+/* ---   Age   --- */
+
+void AGeneratedCube::SetAge(const int32 iAge)
+{
+    Age = iAge;
+    TextAge->SetText(FString::Printf(TEXT("%d"), iAge));
+}
+
+void AGeneratedCube::UpdateLifetime(const int32 iLifetime)
+{
+    if (iLifetime < 0)
+        this->Destroy();
+    else
+    {
+        TextLifetime->SetText(FString::Printf(TEXT("%d"), iLifetime));
+        UpdateLifetime_BP(iLifetime);
+    }
+}
+
+void AGeneratedCube::UpdateLifetime_BP_Implementation(const int32 iLifetime)
+{
+    // in BP
+}
+//----------------------------------------------------------------------------------------
+
+
+
+/* ---   Color   --- */
+
+void AGeneratedCube::SetColor(const FLinearColor iColor)
+{
+    CubeMesh->CreateDynamicMaterialInstance(0)->SetVectorParameterValue(TEXT("CubeColor"), iColor);
+}
+//----------------------------------------------------------------------------------------
---
+++ Source/M2PW10/GeneratedCube.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Components/TextRenderComponent.h"
+
+#include "GeneratedCube.generated.h"
+
+UCLASS()
+class M2PW10_API AGeneratedCube : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    /* ---   Base   --- */
+
+    AGeneratedCube();
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = Components)
+    UStaticMeshComponent *CubeMesh = nullptr;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = Components)
+    UTextRenderComponent *TextAge;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = Components)
+    UTextRenderComponent *TextLifetime;
+    //--------------------------------------------
+
+protected:
+
+    virtual void BeginPlay() override;
+
+public:
+
+    virtual void Tick(float DeltaTime) override;
+
+
+
+    /* ---   Age   --- */
+
+    // Возвраст кубика: время жизни в секундах
+    int32 Age = 0;
+
+    UFUNCTION(BlueprintCallable)
+    void SetAge(const int32 iAge);
+
+    void UpdateLifetime(const int32 iLifetime);
+    UFUNCTION(BlueprintNativeEvent)
+    void UpdateLifetime_BP(const int32 iLifetime);
+    //--------------------------------------------
+
+
+
+    /* ---   Color   --- */
+
+    UFUNCTION(BlueprintCallable)
+    void SetColor(const FLinearColor iColor);
+    //--------------------------------------------
+};
